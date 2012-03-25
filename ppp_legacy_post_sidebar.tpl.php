92b6c6a5bd367352fccdf4bb2e5bda26d654f3d3
Christoph Burschka <christoph@burschka.de>
2012-03-25 23:22:31 +0300

    Initial

---
+++ ppp_legacy_post_sidebar.tpl.php
@@ -0,0 +1,3 @@
+<p class="ppp-username"><?php print l($user->pdn, 'member/' . $user->uid) ?></p>
+<p class="ppp-usertitle"><?php print $user->title ?></p>
+<p class="ppp-uid">Member # <?php print $user->uid ?></p>
