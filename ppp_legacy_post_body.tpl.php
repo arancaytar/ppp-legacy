394683ac0bc74d2be8b91f2ce6e7b5184bfbb088
Christoph Burschka <christoph@burschka.de>
2012-04-01 01:07:42 +0300

    Final push

--- ppp_legacy_post_body.tpl.php
+++ ppp_legacy_post_body.tpl.php
@@ -1,11 +1,11 @@
-<div class="ppp-icon-<?php print $post->icon ?>"></div>
+<span class="ppp-icon ppp-icon-<?php print $post->icon ?>"></span>
 <span class="ppp-postdate">written <?php print format_date($post->post_date, 'custom', 'l, F j Y H:i') ?></span>
-<a class="ppp-icon-profile" href="<?php print url('member/' . $user->uid) ?>" alt="Profile" title="View Profile"></a>
-<a class="ppp-icon-homepage" href="<?php print $user->homepage ?>" alt="Homepage" title="View Homepage"></a>
+<a class="ppp-icon ppp-icon-profile" href="<?php print url('member/' . $user->uid) ?>" alt="Profile" title="View Profile"></a>
+<a class="ppp-icon ppp-icon-homepage" href="<?php print $user->homepage ?>" alt="Homepage" title="View Homepage"></a>
 <span id="<?php print str_pad($post->pid, 6, 0) ?>">#<?php print $post->pid ?></span>
-<hr class="ppp-rule" />
-<?php $editline = '<small>[ ' . date('l, F d, Y H:i', $post->edit_date) . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ?>
+<hr size="1" class="ppp-rule" />
+<?php $editline = $post->edit_date ? ('<small>[ ' . format_date($post->edit_date, 'custom', 'l, F d, Y H:i') . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ) : '' ?>
 <?php print str_replace('[edit]', $editline, check_markup($post->body, variable_get('ppp_legacy_post_format', 'xbbcode'))) ?>
-<hr class="ppp-rule" />
-<span class="ppp-postcount">Posts: <strong><?php print l($user->posts, 'member/' . $user->uid . '/posts') ?></strong></span> | 
+<hr size="1" class="ppp-rule" />
+<span class="ppp-postcount">Posts: <strong><?php print l($user->posts, 'member/' . $user->uid) ?></strong></span> | 
 <span class="ppp-registered">Registered: <strong><?php print format_date($user->joined, 'custom', 'l, F j Y H:i') ?></strong>
--- ppp_legacy_post_sidebar.tpl.php
+++ ppp_legacy_post_sidebar.tpl.php
@@ -1,3 +1,3 @@
-<p class="ppp-username"><?php print l($user->pdn, 'member/' . $user->uid) ?></p>
-<p class="ppp-usertitle"><?php print $user->title ?></p>
-<p class="ppp-uid">Member # <?php print $user->uid ?></p>
+<div class="ppp-username"><?php print l($user->pdn, 'member/' . $user->uid) ?></div>
+<div class="ppp-usertitle"><?php print $user->title ?></div>
+<div class="ppp-uid">Member # <?php print $user->uid ?></div>
