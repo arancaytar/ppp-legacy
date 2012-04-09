fe708f7c502fba959c6ce8edeaa8ba17b7772aa6
Christoph Burschka <christoph@burschka.de>
2012-04-09 04:23:15 -0700

    Link to proper page and fragment from the user posts page.

--- ppp_legacy_post_body.tpl.php
+++ ppp_legacy_post_body.tpl.php
@@ -2,7 +2,7 @@
 <span class="ppp-postdate">written <?php print format_date($post->post_date, 'custom', 'l, F j Y H:i') ?></span>
 <a class="ppp-icon ppp-icon-profile" href="<?php print url('member/' . $user->uid) ?>" alt="Profile" title="View Profile"></a>
 <a class="ppp-icon ppp-icon-homepage" href="<?php print $user->homepage ?>" alt="Homepage" title="View Homepage"></a>
-<span id="<?php print str_pad($post->pid, 6, 0) ?>">#<?php print $post->pid ?></span>
+<a name="<?php print sprintf('%06d', $post->pid) ?>" id="<?php print $post->pid; ?>">#<?php print $post->pid ?></a>
 <hr size="1" class="ppp-rule" />
 <?php $editline = $post->edit_date ? ('<small>[ ' . format_date($post->edit_date, 'custom', 'l, F d, Y H:i') . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ) : '' ?>
 <?php print str_replace('[edit]', $editline, check_markup($post->body, variable_get('ppp_legacy_post_format', 'xbbcode'))) ?>
