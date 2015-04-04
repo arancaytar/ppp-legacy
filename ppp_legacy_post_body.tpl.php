65e009338960ed703f0732b6de75e23022137dde
Christoph Burschka <christoph@burschka.de>
2015-04-04 05:28:41 +0200

    <a name> is deprecated. Use the id attribute.

--- ppp_legacy_post_body.tpl.php
+++ ppp_legacy_post_body.tpl.php
@@ -2,7 +2,7 @@
 <span class="ppp-postdate">written <?php print format_date($post->post_date, 'custom', 'l, F j Y H:i') ?></span>
 <a class="ppp-icon ppp-icon-profile" href="<?php print url('member/' . $user->uid) ?>" title="View Profile"><span class="alt-text">Profile</span></a>
 <a class="ppp-icon ppp-icon-homepage" href="<?php print $user->homepage ?>" title="View Homepage"><span class="alt-text">Homepage</span></a>
-<a name="<?php print sprintf('%06d', $post->pid) ?>" id="<?php print $post->pid; ?>">#<?php print $post->pid ?></a>
+<a id="<?php print sprintf('%06d', $post->pid) ?>">#<?php print $post->pid ?></a>
 <hr size="1" class="ppp-rule" />
 <?php $editline = $post->edit_date ? ('<small>[ ' . format_date($post->edit_date, 'custom', 'l, F d, Y H:i') . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ) : '' ?>
 <?php print str_replace('[edit]', $editline, check_markup($post->body, variable_get('ppp_legacy_post_format', 'xbbcode'))) ?>
