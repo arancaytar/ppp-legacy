a356a30cf997abc81468284b454dfafad4faa364
Christoph Burschka <christoph@burschka.de>
2015-04-04 06:21:03 +0200

    Replace <a> with <span> in post ID.
    Without the deprecated name attribute, the <a> element is
    superfluous for non-link anchors.
    
    Also class it as a ppp-icon to get proper spacing.

--- ppp_legacy_post_body.tpl.php
+++ ppp_legacy_post_body.tpl.php
@@ -4,7 +4,7 @@
 <?php if (valid_url($user->homepage, TRUE)) : ?>
 <a class="ppp-icon ppp-icon-homepage" href="<?php print $user->homepage ?>" title="View Homepage"><span class="alt-text">Homepage</span></a>
 <?php endif; ?>
-<a<?php if ($in_topic) print ' id="' . sprintf('%06d', $post->pid) . '"' ?>>#<?php print $post->pid ?></a>
+<span class="ppp-icon" <?php if ($in_topic) print ' id="' . sprintf('%06d', $post->pid) . '"' ?>>#<?php print $post->pid ?></span>
 <hr class="ppp-rule" />
 <?php $editline = $post->edit_date ? ('<small>[ ' . format_date($post->edit_date, 'custom', 'l, F d, Y H:i') . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ) : '' ?>
 <?php print str_replace('[edit]', $editline, check_markup($post->body, variable_get('ppp_legacy_post_format', 'xbbcode'))) ?>
