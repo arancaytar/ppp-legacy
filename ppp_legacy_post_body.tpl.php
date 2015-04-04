1abab80802f0ad99cc307de6aeb74990a05a1d66
Christoph Burschka <christoph@burschka.de>
2015-04-04 06:20:28 +0200

    Validate homepage URL before printing it out as a link.

--- ppp_legacy_post_body.tpl.php
+++ ppp_legacy_post_body.tpl.php
@@ -1,7 +1,9 @@
 <span class="ppp-icon ppp-icon-<?php print $post->icon ?>"></span>
 <span class="ppp-postdate">written <?php print format_date($post->post_date, 'custom', 'l, F j Y H:i') ?></span>
 <a class="ppp-icon ppp-icon-profile" href="<?php print url('member/' . $user->uid) ?>" title="View Profile"><span class="alt-text">Profile</span></a>
+<?php if (valid_url($user->homepage, TRUE)) : ?>
 <a class="ppp-icon ppp-icon-homepage" href="<?php print $user->homepage ?>" title="View Homepage"><span class="alt-text">Homepage</span></a>
+<?php endif; ?>
 <a<?php if ($in_topic) print ' id="' . sprintf('%06d', $post->pid) . '"' ?>>#<?php print $post->pid ?></a>
 <hr class="ppp-rule" />
 <?php $editline = $post->edit_date ? ('<small>[ ' . format_date($post->edit_date, 'custom', 'l, F d, Y H:i') . ': Message edited by: ' . ($post->edit_name) . ' ]</small>' ) : '' ?>
